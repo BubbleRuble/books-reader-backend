4f713ad6632849505003e88aacd8ad0e1c66f720
Andriy <jeki4an22@gmail.com>
2025-08-12 16:08:31 +0300

    mongoose приколи і схеми

---
+++ models/book.js
@@ -0,0 +1,57 @@
+const { required } = require('joi');
+const { Schema, model } = require('mongoose');
+const { handleMongooseError } = require('../helpers');
+
+const bookGenres = ['fantasy', 'triller', 'drama', 'detective', 'horror', 'romance'];
+const dataRegexp = /^\d{2}-\d{2}-\d{4}$/;
+
+const bookSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    genre: {
+      type: String,
+      enum: bookGenres,
+      required: true,
+    },
+    date: {
+      type: String,
+      match: dataRegexp,
+      required: true,
+    },
+  },
+  { versionKey: false, timestamps: true },
+);
+
+bookSchema.post('save', handleMongooseError);
+
+const Book = model('book', bookSchema);
+
+const Joi = require('joi');
+
+const addSchema = Joi.object({
+  title: Joi.string().required(),
+  author: Joi.string().required(),
+  favorite: Joi.boolean(),
+  genre: Joi.string().valid(...bookGenres).required(),
+  date: Joi.string().pattern(dataRegexp).required(),
+});
+
+const schemas = {
+  addSchema,
+}
+
+module.exports = { 
+  Book, 
+  schemas,
+};
--- routes/api/books.js
+++ routes/api/books.js
@@ -4,18 +4,18 @@ const ctrl = require("../../controllers/books");
 
 const {validateBody} = require("../../middlewares");
 
-const schemas = require("../../schemas/books");
+const {schemas} = require('../../models/book');
 
 const router = express.Router();
 
 router.get("/", ctrl.getAll);
 
-router.get("/:id", ctrl.getById);
+// router.get("/:id", ctrl.getById);
 
 router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.put("/:id",  validateBody(schemas.addSchema), ctrl.updateById);
+// router.put("/:id",  validateBody(schemas.addSchema), ctrl.updateById);
 
-router.delete("/:id", ctrl.deleteById);
+// router.delete("/:id", ctrl.deleteById);
 
 module.exports = router;
